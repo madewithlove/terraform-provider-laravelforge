4d667dd4e07417045a6eab56881ac20d3582967a
Andreas Creten <andreas@madewithlove.be>
2023-03-31 13:45:59 +0200

    Scaffolding with the Terraform Provider Framework

---
+++ internal/provider/provider.go
@@ -0,0 +1,80 @@
+package provider
+
+import (
+	"context"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/provider"
+	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+
+	ForgeClient "github.com/madwithlove/forge-go-sdk/client"
+)
+
+// Ensure LaravelForgeProvider satisfies various provider interfaces.
+var _ provider.Provider = &LaravelForgeProvider{}
+
+// LaravelForgeProvider defines the provider implementation.
+type LaravelForgeProvider struct {
+	// version is set to the provider version on release, "dev" when the
+	// provider is built and ran locally, and "test" when running acceptance
+	// testing.
+	version string
+}
+
+// LaravelForgeProviderModel describes the provider data model.
+type LaravelForgeProviderModel struct {
+	Endpoint types.String `tfsdk:"token"`
+}
+
+func (p *LaravelForgeProvider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
+	resp.TypeName = "forge"
+	resp.Version = p.version
+}
+
+func (p *LaravelForgeProvider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"token": schema.StringAttribute{
+				MarkdownDescription: "Laravel Forge API token.",
+				Optional:            false,
+			},
+		},
+	}
+}
+
+func (p *LaravelForgeProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+	var data LaravelForgeProviderModel
+
+	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	client := ForgeClient.NewAPIClient()
+
+	client.cliet.set_default_header("Authorization", "Bearer YOUR_BEARER_TOKEN")
+	resp.DataSourceData = client
+	resp.ResourceData = client
+}
+
+func (p *LaravelForgeProvider) Resources(ctx context.Context) []func() resource.Resource {
+	return []func() resource.Resource{
+		NewResourceServer,
+	}
+}
+
+func (p *LaravelForgeProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
+	return []func() datasource.DataSource{
+		NewExampleDataSource,
+	}
+}
+
+func New(version string) func() provider.Provider {
+	return func() provider.Provider {
+		return &LaravelForgeProvider{
+			version: version,
+		}
+	}
+}
---
+++ internal/provider/resource_server.go
@@ -0,0 +1,190 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+)
+
+// Ensure provider defined types fully satisfy framework interfaces.
+var _ resource.Resource = &ResourceServer{}
+var _ resource.ResourceWithImportState = &ResourceServer{}
+
+func NewResourceServer() resource.Resource {
+	return &ResourceServer{}
+}
+
+// ResourceServer defines the resource implementation.
+type ResourceServer struct {
+	client *http.Client
+}
+
+// ServerResourceModel describes the resource data model.
+type ServerResourceModel struct {
+	Id           types.Int64  `tfsdk:"id"`
+	Platform     types.String `tfsdk:"platform"`
+	CredentialId types.String `tfsdk:"credential_id"`
+}
+
+func (r *ResourceServer) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_server"
+}
+
+func (r *ResourceServer) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		MarkdownDescription: "Server resource",
+
+		Attributes: map[string]schema.Attribute{
+			"id": schema.Int64Attribute{
+				Computed:            true,
+				MarkdownDescription: "Server identifier",
+				PlanModifiers: []planmodifier.Int64{
+					int64planmodifier.UseStateForUnknown(),
+				},
+			},
+			"platform": schema.StringAttribute{
+				MarkdownDescription: "The server provider. Valid values are `ocean2` for Digital Ocean, `linode4`, `vultr2`, `aws`, `hetzner` and `custom`.",
+				Optional:            false,
+				Validators: []validator.String{
+					stringvalidator.OneOf("ocean2", "linode4", "vultr2", "aws", "hetzner", "custom"),
+				},
+			},
+			"type": schema.StringAttribute{
+				MarkdownDescription: "The type of server to create. Valid values are `app`, `web`, `loadbalancer`, `cache`, `database`, `worker`, `meilisearch`. `app` is used by default if no value is defined.",
+				Optional:            true,
+				Validators: []validator.String{
+					stringvalidator.OneOf("app", "web", "loadbalancer", "cache", "database", "worker", "meilisearch"),
+				},
+			},
+		},
+	}
+}
+
+func (r *ResourceServer) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(*http.Client)
+
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Resource Configure Type",
+			fmt.Sprintf("Expected *http.Client, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+
+		return
+	}
+
+	r.client = client
+}
+
+func (r *ResourceServer) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var data *ServerResourceModel
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// If applicable, this is a great opportunity to initialize any necessary
+	// provider client data and make a call using it.
+	// httpResp, err := r.client.Do(httpReq)
+	// if err != nil {
+	//     resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to create example, got error: %s", err))
+	//     return
+	// }
+
+	context.Context().Value()
+
+	// For the purposes of this example code, hardcoding a response value to
+	// save into the Terraform state.
+	data.Id = types.Int64Value("example-id")
+
+	// Write logs using the tflog package
+	// Documentation: https://terraform.io/plugin/log
+	tflog.Trace(ctx, "Created a server")
+
+	// Save data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *ResourceServer) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var data *ServerResourceModel
+
+	// Read Terraform prior state data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// If applicable, this is a great opportunity to initialize any necessary
+	// provider client data and make a call using it.
+	// httpResp, err := r.client.Do(httpReq)
+	// if err != nil {
+	//     resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to read example, got error: %s", err))
+	//     return
+	// }
+
+	// Save updated data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *ResourceServer) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var data *ServerResourceModel
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// If applicable, this is a great opportunity to initialize any necessary
+	// provider client data and make a call using it.
+	// httpResp, err := r.client.Do(httpReq)
+	// if err != nil {
+	//     resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to update example, got error: %s", err))
+	//     return
+	// }
+
+	// Save updated data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *ResourceServer) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var data *ServerResourceModel
+
+	// Read Terraform prior state data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// If applicable, this is a great opportunity to initialize any necessary
+	// provider client data and make a call using it.
+	// httpResp, err := r.client.Do(httpReq)
+	// if err != nil {
+	//     resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to delete example, got error: %s", err))
+	//     return
+	// }
+}
+
+func (r *ResourceServer) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
+}
